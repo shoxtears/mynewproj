9578d3d4caeebe1ce3c395fd603f179f383e5a07
spo1ledsound <vlad.kachyk@gmail.com>
2020-12-10 19:38:07 +0200

    change cs 2

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace myproject
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!"); gfieohoiohhinkn
+            Console.WriteLine("Hello World!"); gfieohoiohhinkn оиимриріимрилоіивмлот
         }
     }
 }
