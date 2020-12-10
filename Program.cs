de7b7338370bfa00df94fb138b13d95f5ebffd53
spo1ledsound <vlad.kachyk@gmail.com>
2020-12-10 18:46:45 +0200

    new commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myproject
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
